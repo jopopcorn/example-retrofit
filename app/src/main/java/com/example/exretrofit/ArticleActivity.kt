ea726d6d81be1b4d55a5f63d2efe2badb2cfca75
Sooyeon Jo <supop1009@gmail.com>
2021-02-16 23:07:05 +0900

    feat: Add ProgressWebView in ArticleActivity
    - activity_article.xml에 ProgressBar 추가
    
    - ArticleActivity에ProgressBar를 함께 띄우는 ProgressWebView 클래스 생성
    이에 따라 WebView의 WebViewClient를 ProgressWebView로 변경했다.

--- app/src/main/java/com/example/exretrofit/ArticleActivity.kt
+++ app/src/main/java/com/example/exretrofit/ArticleActivity.kt
@@ -1,7 +1,11 @@
 package com.example.exretrofit
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.example.exretrofit.databinding.ActivityArticleBinding
 
@@ -19,8 +23,25 @@ class ArticleActivity : AppCompatActivity() {
 
         if(intent.hasExtra("url")){
             val link = intent.extras?.get("url").toString()
-            binding.webView.webViewClient = WebViewClient()
-            binding.webView.loadUrl(link)
+            binding.webView.apply {
+                webViewClient = ProgressWebView()
+                loadUrl(link)
+            }
+        }
+    }
+
+    inner class ProgressWebView(): WebViewClient(){
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            binding.progressBar.visibility = View.VISIBLE
+            binding.webView.visibility = View.INVISIBLE
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            binding.progressBar.visibility = View.GONE
+            binding.webView.visibility = View.VISIBLE
         }
     }
 }
